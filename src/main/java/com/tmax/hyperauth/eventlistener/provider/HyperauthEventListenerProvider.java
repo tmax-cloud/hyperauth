eeca90a6eb1247f0d18a855106b07d0183ba8d45
dnxorjs1 <taegeon_woo@tmax.co.kr>
2020-12-11 15:03:41 +0900

    test

--- src/main/java/com/tmax/hyperauth/eventlistener/provider/HyperauthEventListenerProvider.java
+++ src/main/java/com/tmax/hyperauth/eventlistener/provider/HyperauthEventListenerProvider.java
@@ -41,8 +41,8 @@ public class HyperauthEventListenerProvider extends TimerSpi implements EventLis
     @Override
     public void onEvent(Event event) {
         String userName = "";
-        logger.info("Event Occurred:" + toString(event));
-        System.out.println("Event Occurred:" + toString(event));
+        logger.info("Logger !!!!!  Event Occurred :" + toString(event));
+        System.out.println("STDout!!! Event Occurred:" + toString(event));
         if (event.getRealmId().equalsIgnoreCase("tmax")) {
             switch (event.getType().toString()) {
                 case "REGISTER":
