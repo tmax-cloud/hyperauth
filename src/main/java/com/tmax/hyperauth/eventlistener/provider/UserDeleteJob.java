7d134adc6df7efc6c225e5ea64f65f11a8a889ce
dnxorjs1 <taegeon_woo@tmax.co.kr>
2020-12-16 16:33:23 +0900

    test

--- src/main/java/com/tmax/hyperauth/eventlistener/provider/UserDeleteJob.java
+++ src/main/java/com/tmax/hyperauth/eventlistener/provider/UserDeleteJob.java
@@ -1,17 +1,27 @@
 package com.tmax.hyperauth.eventlistener.provider;
 
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserModel;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.List;
+
 public class UserDeleteJob implements Job {
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
+
         KeycloakSession session = (KeycloakSession) context.getJobDetail().getJobDataMap().get("session");
+        session.getTransactionManager().begin();
         System.out.println();
         System.out.println(" Timer Wake Up !!!!!");
-        System.out.println(session.users().getUserByEmail("admin@tmax.co.kr", session.realms().getRealmByName("tmax")).getUsername());
+        List<UserModel> users = session.users().getUsers(session.realms().getRealmByName("tmax"),false);
+        for( UserModel user : users) {
+            System.out.println( user.getEmail());
+        }
+
+        session.getTransactionManager().commit();
         System.out.println();
         System.out.println();
         System.out.println();
