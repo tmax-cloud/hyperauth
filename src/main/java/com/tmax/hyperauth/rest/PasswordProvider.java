c104c57e1aadc284b7059326ed3f2fe14abedd3b
dnxorjs1 <taegeon_woo@tmax.co.kr>
2021-02-02 17:56:05 +0900

    [mod] 운영환경 배포용

--- src/main/java/com/tmax/hyperauth/rest/PasswordProvider.java
+++ src/main/java/com/tmax/hyperauth/rest/PasswordProvider.java
@@ -75,7 +75,7 @@ public class PasswordProvider implements RealmResourceProvider {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response get(@QueryParam("email") final String email, @QueryParam("code") String code ,@QueryParam("token") String tokenString,
                         @FormParam("password") String password, @FormParam("confirmPassword") String confirmPassword
-                      ,@QueryParam("password") String password1, @QueryParam("confirmPassword") String confirmPassword1
+//                      ,@QueryParam("password") String password1, @QueryParam("confirmPassword") String confirmPassword1
     ) {
         System.out.println("***** PUT /password");
         try {
@@ -119,8 +119,8 @@ public class PasswordProvider implements RealmResourceProvider {
                 return Util.setCors(status, out);
             }
 
-            if(password1!= null) password = password1;
-            if(confirmPassword1!= null) confirmPassword = confirmPassword1;
+//            if(password1!= null) password = password1;
+//            if(confirmPassword1!= null) confirmPassword = confirmPassword1;
 
             if (StringUtil.isEmpty(password)) {
                 status = Status.BAD_REQUEST;
@@ -178,32 +178,32 @@ public class PasswordProvider implements RealmResourceProvider {
         }
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response get( @QueryParam("userId") String userId,  @QueryParam("password") String password) {
-        System.out.println("***** Verify /password");
-        if ( StringUtil.isEmpty(userId)){
-            status = Status.BAD_REQUEST;
-            out = "User Id is Empty";
-            return Util.setCors(status, out);
-        }
-        if ( StringUtil.isEmpty(password)){
-            status = Status.BAD_REQUEST;
-            out = "Password is Empty";
-            return Util.setCors(status, out);
-        }
-        RealmModel realm = session.realms().getRealmByName("tmax");
-        UserModel user = session.users().getUserByEmail(userId, realm);
-        UserCredentialModel cred = UserCredentialModel.password(password);
-        if (session.userCredentialManager().isValid(realm, user, cred)) {
-            status = Status.OK;
-            out = "Password is Correct";
-        } else {
-            status = Status.BAD_REQUEST;
-            out = "Password is Wrong";
-        }
-        return Util.setCors(status, out);
-    }
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response get( @QueryParam("userId") String userId,  @QueryParam("password") String password) {
+//        System.out.println("***** Verify /password");
+//        if ( StringUtil.isEmpty(userId)){
+//            status = Status.BAD_REQUEST;
+//            out = "User Id is Empty";
+//            return Util.setCors(status, out);
+//        }
+//        if ( StringUtil.isEmpty(password)){
+//            status = Status.BAD_REQUEST;
+//            out = "Password is Empty";
+//            return Util.setCors(status, out);
+//        }
+//        RealmModel realm = session.realms().getRealmByName("tmax");
+//        UserModel user = session.users().getUserByEmail(userId, realm);
+//        UserCredentialModel cred = UserCredentialModel.password(password);
+//        if (session.userCredentialManager().isValid(realm, user, cred)) {
+//            status = Status.OK;
+//            out = "Password is Correct";
+//        } else {
+//            status = Status.BAD_REQUEST;
+//            out = "Password is Wrong";
+//        }
+//        return Util.setCors(status, out);
+//    }
 
     @PATCH
     @Produces(MediaType.APPLICATION_JSON)
