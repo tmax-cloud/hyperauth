887c2be8feb1f4fc6708b4df34e33646e543dc27
dnxorjs1 <taegeon_woo@tmax.co.kr>
2021-02-16 12:31:36 +0900

    탈퇴신청 테스트 코드 삽입

--- src/main/java/com/tmax/hyperauth/rest/UserProvider.java
+++ src/main/java/com/tmax/hyperauth/rest/UserProvider.java
@@ -415,7 +415,7 @@ public class UserProvider implements RealmResourceProvider {
                         String subject = "[Tmax 통합서비스] 고객님의 계정 탈퇴 신청이 완료되었습니다.";
 //                        String body = Constants.ACCOUNT_WITHDRAWAL_REQUEST_BODY;
 
-                        String body = Util.readLineByLineJava8("/opt/jboss/keycloak/themes/tmax/email/html/etc/account-withdrawal-request.html");
+//                        String body = Util.readLineByLineJava8("/opt/jboss/keycloak/themes/tmax/email/html/etc/account-withdrawal-request.html"); // TODO
 //                        List<Util.MailImage> imageParts = new ArrayList<>(
 //                                Arrays.asList(
 //                                        new Util.MailImage( "/opt/jboss/keycloak/themes/tmax/email/html/resources/img/logo_tmax.svg","logo_tmax.svg" ),
@@ -423,6 +423,16 @@ public class UserProvider implements RealmResourceProvider {
 //                                        new Util.MailImage( "/opt/jboss/keycloak/themes/tmax/email/html/resources/img/bg.svg","bg.svg" )
 //                                )
 //                        );
+
+                        //FIXME : TESTCODE
+                        String body = Util.readLineByLineJava8("/opt/jboss/keycloak/themes/tmax/email/html/etc/account-withdrawal-completed.html");
+                        List<Util.MailImage> imageParts = new ArrayList<>(
+                                Arrays.asList(
+                                        new Util.MailImage( "/opt/jboss/keycloak/themes/tmax/email/html/resources/img/logo_tmax.svg","logo_tmax.svg" ),
+                                        new Util.MailImage( "/opt/jboss/keycloak/themes/tmax/email/html/resources/img/secession_success.svg","secession_success.svg" ),
+                                        new Util.MailImage( "/opt/jboss/keycloak/themes/tmax/email/html/resources/img/bg.svg","bg.svg" )
+                                )
+                        );
 
                         Util.sendMail(session, email, subject, body, null );
                         status = Status.OK;
