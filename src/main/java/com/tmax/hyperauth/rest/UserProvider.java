a979680198b500def680fd0b6d036898e101aa5a
taegeon_woo <taegeon_woo@tmax.co.kr>
2021-02-24 10:45:51 +0900

    유저 탈퇴 메일 테스트 by 신은경

--- src/main/java/com/tmax/hyperauth/rest/UserProvider.java
+++ src/main/java/com/tmax/hyperauth/rest/UserProvider.java
@@ -462,7 +462,7 @@ public class UserProvider implements RealmResourceProvider {
 //                        userModel.setEnabled(false);  //유저 탈퇴 철회 시나리오로 인해서 삭제
                         String email = userModel.getEmail();
                         String subject = "[Tmax 통합계정] 고객님의 계정 탈퇴 신청이 완료되었습니다.";
-                        String body = Constants.ACCOUNT_WITHDRAWAL_REQUEST_BODY;
+//                        String body = Constants.ACCOUNT_WITHDRAWAL_REQUEST_BODY;
 
 //                        String body = Util.readLineByLineJava8("/opt/jboss/keycloak/themes/tmax/email/html/etc/account-withdrawal-request.html"); // TODO
 //                        List<Util.MailImage> imageParts = new ArrayList<>(
@@ -473,8 +473,8 @@ public class UserProvider implements RealmResourceProvider {
 //                                )
 //                        );
 
-//                        //FIXME : TESTCODE
-//                        String body = Util.readLineByLineJava8("/opt/jboss/keycloak/themes/tmax/email/html/etc/account-withdrawal-completed.html");
+                        //FIXME : TESTCODE
+                        String body = Util.readLineByLineJava8("/opt/jboss/keycloak/themes/tmax/email/html/etc/account-withdrawal-completed.html");
 //                        List<Util.MailImage> imageParts = new ArrayList<>(
 //                                Arrays.asList(
 //                                        new Util.MailImage( "/opt/jboss/keycloak/themes/tmax/email/html/resources/img/logo_tmax.png","logo_tmax" ),
