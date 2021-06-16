e6c5dddc528f147f16430b05da1362e3f00139b8
dnxorjs1 <taegeon_woo@tmax.co.kr>
2021-06-16 14:52:42 +0900

    test

--- src/main/java/com/tmax/hyperauth/eventlistener/provider/Producer.java
+++ src/main/java/com/tmax/hyperauth/eventlistener/provider/Producer.java
@@ -13,40 +13,39 @@ import java.util.Properties;
 @Slf4j
 public class Producer {
     private final static String BOOTSTRAP_SERVER = "kafka-1."+System.getenv("NAMESPACE")+":9092,kafka-2."+System.getenv("NAMESPACE")+":9092,kafka-3."+System.getenv("NAMESPACE")+":9092";
-//    private final static String BOOTSTRAP_SERVER_EXTERNAL = "172.22.6.9:9092,172.22.6.10:9092,172.22.6.11:9092";
     public static void publishEvent(String topic, Object value){
-//        //reset thread context
-//        resetThreadContext();
-//
-//        // create the producer
-//        KafkaProducer<String, String> producer = new KafkaProducer<>(getProperties());
-//
-//        // create a producer record
-//        Gson gson = new Gson();
-//        String jsonValue = gson.toJson(value);
-//        ProducerRecord<String, String> eventRecord = new ProducerRecord<String, String>(topic, jsonValue);
-//
-//        // Async Call producer.send
-//        new Thread(
-//                () -> {
-//                    try {
-//                        log.info("kafka produce start");
-//                        producer.send(eventRecord, (metadata, exception) -> {
-//                            if (exception != null) {
-//                                log.error("Error Occurs!!", exception);
-//                                log.error("Failed to Send to Topic Server");
-//                            } else {
-//                                log.info("kafka produce success");
-//                            }
-//                        });
-//                    } catch (Exception e) {
-//                        log.error("Error Occurs!!", e);
-//                    } finally {
-//                        producer.flush();
-//                        producer.close();
-//                    }
-//                }
-//        ).start();
+        //reset thread context
+        resetThreadContext();
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(getProperties());
+
+        // create a producer record
+        Gson gson = new Gson();
+        String jsonValue = gson.toJson(value);
+        ProducerRecord<String, String> eventRecord = new ProducerRecord<String, String>(topic, jsonValue);
+
+        // Async Call producer.send
+        new Thread(
+                () -> {
+                    try {
+                        log.info("kafka produce start");
+                        producer.send(eventRecord, (metadata, exception) -> {
+                            if (exception != null) {
+                                log.error("Error Occurs!!", exception);
+                                log.error("Failed to Send to Topic Server");
+                            } else {
+                                log.info("kafka produce success");
+                            }
+                        });
+                    } catch (Exception e) {
+                        log.error("Error Occurs!!", e);
+                    } finally {
+                        producer.flush();
+                        producer.close();
+                    }
+                }
+        ).start();
     }
 
     private static void resetThreadContext() {
@@ -68,12 +67,6 @@ public class Producer {
         properties.setProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, System.getenv("CERTS_PASSWORD"));
         properties.setProperty(SslConfigs.SSL_KEY_PASSWORD_CONFIG, System.getenv("CERTS_PASSWORD"));
 
-//        // for SASL_OAUTH_BEARER
-//        properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "kafka-producer");
-//        properties.setProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
-//        properties.setProperty(SaslConfigs.SASL_MECHANISM, "OAUTHBEARER");
-//        properties.setProperty(SaslConfigs.SASL_LOGIN_CALLBACK_HANDLER_CLASS, "com.bfm.kafka.security.oauthbearer.OAuthAuthenticateLoginCallbackHandler");
-//        properties.setProperty(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required;");
         return properties;
     }
 
