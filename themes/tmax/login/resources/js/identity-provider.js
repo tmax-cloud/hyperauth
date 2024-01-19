f243f84b18a2701512c286f524b50e6bbdfb80fd
2smin <planner263@gmail.com>
2024-01-19 14:51:43 +0900

    change service jar

--- themes/tmax/login/resources/js/identity-provider.js
+++ themes/tmax/login/resources/js/identity-provider.js
@@ -210,11 +210,7 @@ function sendVerificationEmailForNewAccount() {
 
   var inputFirstName = document.getElementById("input-first-name").value
   var inputLastName = document.getElementById("input-last-name").value
-
-  console.log("input-first-name", inputFirstName)
-  console.log("input-last-name", inputLastName)
-
-  idpForm.querySelector("#firstName").setVa
+  idpForm.querySelector("#firstName").value = inputFirstName;
   idpForm.querySelector("#lastName").value = inputLastName;
 
   idpForm.submit();
